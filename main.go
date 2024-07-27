f146a5ca4fb49abdd3c07d5bd4fecd68781e4299
manuelrojas19 <manuelarr99@gmail.com>
2024-07-27 00:24:38 -0600

    Updated main for new services and handlers

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/manuelrojas19/go-oauth2-server/api/handlers"
 	"github.com/manuelrojas19/go-oauth2-server/configuration/database"
+	"github.com/manuelrojas19/go-oauth2-server/configuration/googleauth"
 	"github.com/manuelrojas19/go-oauth2-server/configuration/keymanager"
+	"github.com/manuelrojas19/go-oauth2-server/configuration/session"
 	"github.com/manuelrojas19/go-oauth2-server/services"
+	"github.com/manuelrojas19/go-oauth2-server/store/repositories"
 	"log"
 	"net/http"
-
-	"github.com/manuelrojas19/go-oauth2-server/store/repositories"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Error loading .env file: %v\n", err)
+		return
+	}
+	// Load Google Secrets
+	googleauth.LoadSecrets()
 	// Initialize database connection
 	db, err := database.InitDatabaseConnection()
 	if err != nil {
@@ -25,6 +36,10 @@ func main() {
 		log.Fatalf("Failed to initialize keys: %v", err)
 	}
 
+	redisClient := session.NewRedisClient()
+
+	userSessionService := services.NewSessionService(redisClient)
+
 	// Initialize repositories and services
 	oauthClientRepository := repositories.NewOauthClientRepository(db)
 	accessTokenRepository := repositories.NewAccessTokenRepository(db)
@@ -34,12 +49,15 @@ func main() {
 	userConsentService := services.NewUserConsentService(userConsentRepository)
 	oauthClientService := services.NewOauthClientService(oauthClientRepository)
 	tokenService := services.NewTokenService(accessTokenRepository, refreshTokenRepository, oauthClientService)
-	authorizationService := services.NewAuthorizationService(oauthClientService, userConsentService, authorizationRepository)
+	authorizationService := services.NewAuthorizationService(oauthClientService, userConsentService, authorizationRepository, userSessionService)
 	wellKnownService := services.NewWellKnownService()
 	registerHandler := handlers.NewRegisterHandler(oauthClientService)
 	tokenHandler := handlers.NewTokenHandler(tokenService)
 	jwksHandler := handlers.NewJwksHandler(wellKnownService)
 	authorizeHandler := handlers.NewAuthorizeHandler(authorizationService)
+	requestConsentHandler := handlers.NewRequestConsentHandler()
+	googleLoginHandler := handlers.NewGoogleLoginHandler()
+	googleAuthorizeCallbackHandler := handlers.NewGoogleAuthorizeCallbackHandler(userSessionService)
 	log.Println("Services and handlers initialized successfully")
 
 	// Setup HTTP handler
@@ -47,6 +65,10 @@ func main() {
 	http.HandleFunc("/oauth/token", tokenHandler.Handler)
 	http.HandleFunc("/oauth/authorize", authorizeHandler.Handler)
 	http.HandleFunc("/.well-known/jwks.json", jwksHandler.Handler)
+	http.HandleFunc("/oauth/consent", requestConsentHandler.Handler)
+	http.HandleFunc("/google/authorize", googleLoginHandler.Handler)
+	http.HandleFunc("/google/authorize/callback", googleAuthorizeCallbackHandler.Handler)
+
 	log.Println("HTTP handler for /register is set up")
 	log.Println("HTTP handler for /oauth/token is set up")
 	log.Println("HTTP handler for /.well-known/jwks.json is set up")
