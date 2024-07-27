d37b9dc525a67be37aac09b5ed46db883bcaa8bc
manuelrojas19 <manuelarr99@gmail.com>
2024-07-27 00:20:49 -0600

    Using google as IDP for User Authentication

---
+++ api/handlers/google_authorize_handler.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/manuelrojas19/go-oauth2-server/configuration/googleauth"
+	"net/http"
+	"net/url"
+)
+
+type googleAuthorizeHandler struct {
+}
+
+func NewGoogleLoginHandler() Handler {
+	return &googleAuthorizeHandler{}
+}
+
+func (g googleAuthorizeHandler) Handler(writer http.ResponseWriter, request *http.Request) {
+	originalParams := map[string]string{
+		"client_id":     request.URL.Query().Get("client_id"),
+		"scope":         request.URL.Query().Get("scope"),
+		"redirect_uri":  request.URL.Query().Get("redirect_uri"),
+		"response_type": request.URL.Query().Get("response_type"),
+	}
+
+	encodedParams, err := encodeState(originalParams)
+	if err != nil {
+		http.Error(writer, "Failed to encode state", http.StatusInternalServerError)
+		return
+	}
+
+	authURL := fmt.Sprintf("%s?client_id=%s&redirect_uri=%s&response_type=code&scope=%s&state=%s",
+		googleauth.GoogleAuthURL, googleauth.GoogleClientID, url.QueryEscape(googleauth.GoogleRedirectURL), googleauth.Scopes, encodedParams)
+	http.Redirect(writer, request, authURL, http.StatusTemporaryRedirect)
+}
+
+func encodeState(state map[string]string) (string, error) {
+	stateJSON, err := json.Marshal(state)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(stateJSON), nil
+}
