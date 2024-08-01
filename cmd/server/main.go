a8dab3cf8adde5f4f9b4342f0be60934892882f0
manuelrojas19 <manuelarr99@gmail.com>
2024-08-01 07:57:28 -0600

    Documentation added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,15 +10,29 @@ import (
 	"go.uber.org/fx"
 )
 
+// main is the entry point of the application. It uses the fx framework to set up and run the application with various modules and dependencies.
 func main() {
+	// Create a new fx.App with the specified modules and invoke functions
 	app := fx.New(
-
+		// Include the configuration module which provides application configuration settings
 		configuration.Module,
+
+		// Include the session module which handles session management
 		session.Module,
+
+		// Include the repositories module which provides data access layer (DAL) functionality
 		repositories.Module,
+
+		// Include the services module which contains business logic and services
 		services.Module,
+
+		// Include the handlers module which defines the HTTP handlers for the application
 		handlers.Module,
+
+		// Invoke the SetupHTTPServer function from the internal package to set up the HTTP server
 		fx.Invoke(internal.SetupHTTPServer),
 	)
+
+	// Run the fx.App, starting the application and its dependencies
 	app.Run()
 }
--- configuration/module.go
+++ configuration/module.go
@@ -6,18 +6,36 @@ import (
 	"go.uber.org/zap"
 )
 
+// Module this module defines a set of options for the fx application, including logger configuration,
+// provided dependencies, and functions to be invoked at startup.
 var Module = fx.Options(
+	// Configure the logger for the fx application using a custom Zap logger
 	fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+		// Return an fxevent.Logger that uses the provided Zap logger
 		return &fxevent.ZapLogger{Logger: log}
 	}),
+
+	// Provide various dependencies to the fx application
 	fx.Provide(
+		// Provide a new database connection
 		NewDatabaseConnection,
+
+		// Provide a new Redis client
 		NewRedisClient,
+
+		// Provide a new HTTP ServeMux
 		NewServeMux,
+
+		// Provide a new logger
 		NewLogger,
 	),
+
+	// Invoke functions to be executed at application startup
 	fx.Invoke(
+		// Load secrets required by the application
 		LoadSecrets,
+
+		// Initialize cryptographic keys
 		InitializeKeys,
 	),
 )
